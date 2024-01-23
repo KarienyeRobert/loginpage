90c690a54e3e3e1e4cdf79d9ec8b253fb1427ece
KarienyeRobert <karienyerobert@gmail.com>
2024-01-23 20:38:49 +0300

    commit

---
+++ script.js
@@ -0,0 +1,6 @@
+const name = document.getElementById('userName');
+const password = document.getElementById('password');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+});
